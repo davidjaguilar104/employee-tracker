c98e136e3aa0397499e3ea2ae738925bd484c880
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-04 14:41:55 -0600

    added user choices prompt

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+
+const promptUserChoices = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "initialOptions",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update an Employee Role",
+      ],
+    },
+  ]);
+};
+
+promptUserChoices();
