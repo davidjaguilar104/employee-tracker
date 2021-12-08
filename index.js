f5ebf835bac75e2247a5f15000ad4266beada8ac
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-08 11:02:11 -0600

    added functions with queries to select all

--- index.js
+++ index.js
@@ -2,29 +2,85 @@ const db = require("./db/connection.js");
 const inquirer = require("inquirer");
 
 function promptUserChoices() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "task",
-      message: "What would you like to do?",
-      choices: [
-        "View All Departments",
-        "View All Roles",
-        "View All Employees",
-        "Add a Department",
-        "Add a Role",
-        "Add an Employee",
-        "Update an Employee Role",
-        "Exit",
-      ],
-    },
-  ]).then(function({ task }) {
-    switch (task) {
-      case "View All Departments":
-      viewDepartments();
-      break;
-    }
-  })
-};
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then(function ({ task }) {
+      switch (task) {
+        case "View All Departments":
+          viewDepartments();
+          break;
+
+        case "View All Roles":
+          viewRoles();
+          break;
+
+        case "View All Employees":
+          viewEmployees();
+          break;
+
+        case "Add a Department":
+          viewDepartments();
+          break;
+
+        case "Add a Role":
+          viewDepartments();
+          break;
+
+        case "Add an Employee":
+          viewDepartments();
+          break;
+
+        case "Update an Employee Role":
+          viewDepartments();
+          break;
+      }
+    });
+}
+
+function viewDepartments() {
+  const sql = `SELECT * FROM departments`;
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+
+    console.table(rows);
+    promptUserChoices();
+  });
+}
+
+function viewRoles() {
+  const sql = `SELECT * FROM roles`;
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+
+    console.table(rows);
+    promptUserChoices();
+  });
+}
+
+function viewEmployees() {
+  const sql = `SELECT * FROM employees`;
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+
+    console.table(rows);
+    promptUserChoices();
+  });
+}
 
 promptUserChoices();
--- queries.js
+++
@@ -1,9 +0,0 @@
-function viewDepartments() {
-    const sql = `SELECT * FROM departments`;
-    db.query(sql, (err, rows) => {
-      if(err) throw err;
-  
-      console.table(rows);
-      promptUserChoices();
-    })
-  }
