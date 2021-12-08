d2a75c340c33b0ef15525db53fa586540cd2dbe5
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-08 09:35:14 -0600

    schema and seeds set up

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO departments (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 1),
+    ('Accountant', 125000, 1),
+    ('Legal Team Lead', 250000, 1),
+    ('Lawyer', 190000, 1);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
+
