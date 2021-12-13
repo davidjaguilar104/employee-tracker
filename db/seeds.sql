b95de69434e41e66b09e11df9ea1958a71fcf36d
davidjaguilar104 <davidjaguilar104@gmail.com>
2021-12-12 18:45:02 -0600

    adjusted seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,10 +12,10 @@ VALUES
     ('Salesperson', 80000, 1),
     ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 1),
-    ('Accountant', 125000, 1),
-    ('Legal Team Lead', 250000, 1),
-    ('Lawyer', 190000, 1);
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
